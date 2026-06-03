994fb89f6131458c7453ed17673a29af5d13da6d
ai-wes <weslagarde@gmail.com>
2026-06-02 18:45:09 -0700

    Update Marketplace image pins for CVE rebuilds

--- preflight-addon/examples/install-addon.sh
+++ preflight-addon/examples/install-addon.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 NAMESPACE="${NAMESPACE:-glassbox-preflight}"
 APP_NAME="${APP_NAME:-glassbox-ipfto-addon}"
 IMAGE_REPO="${IMAGE_REPO:-us-docker.pkg.dev/glassbox-bio-public/glassbox-bio-molecular-ip-fto-screening/glassbox-ipfto}"
-IMAGE_TAG="${IMAGE_TAG:-1.0.0}"
+IMAGE_TAG="${IMAGE_TAG:-1.0.1}"
 
 helm upgrade --install "${APP_NAME}" ./chart/ipfto-addon \
   --namespace "${NAMESPACE}" --create-namespace \
